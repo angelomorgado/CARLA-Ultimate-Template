2c36946be9d6c68bf7a04fa1b32b0f08cdc2d849
AmrMorgado <angelo.morgado@ubi.pt>
2024-02-28 17:08:26 +0000

    added new way of running environment according to open ai gymnasium

--- main.py
+++ main.py
@@ -3,31 +3,40 @@ Main window:
     It acts as the center of the entire program controlling the entire process.
     This depends on the project at hand.
     Various examples of mains that act as tutorials can be found in the examples folder.
+
+    It is possible to call specific scenarios by having a list of them e.g, l = ["Town01-ClearNoon-Road-0", "Town01-WetNight-Road-0"] and then calling them in the reset method as such: env.reset(l[i])
 '''
 
 import time
 from env.environment import CarlaEnv
 
-def env_main():
-    env = CarlaEnv('carla-rl-gym', time_limit=10, initialize_server=False, random_weather=True, synchronous_mode=True, continuous=False, show_sensor_data=False)
-    # l = ["Town01-ClearNoon-Road-0", "Town01-WetNight-Road-0"]
+def steps_main():
+    env = CarlaEnv('carla-rl-gym', time_limit=10, initialize_server=False, random_weather=True, synchronous_mode=True, continuous=False, show_sensor_data=True)
+    obs, info = env.reset()
     
+    # Number of steps
+    for i in range(1000):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        if terminated or truncated:
+            observation, info = env.reset()
+
+    env.close()
+
+def episodes_main():
+    env = CarlaEnv('carla-rl-gym', time_limit=10, initialize_server=False, random_weather=True, synchronous_mode=True, continuous=False, show_sensor_data=True)
+
     for i in range(10):
         print("================================ Episode", i, " ================================")
         obs, info = env.reset()
         while True:
-            # Random action TODO: Test this
             action = env.action_space.sample()
-            # Empty action
-            # action = [0.0, 0.3, 0.0]
-            print('Action:', action)
             obs, reward, terminated, truncated, info = env.step(action)
-            # print('Observation:', obs)
+            
             if terminated or truncated:
                 print('Episode terminated closing environment')
                 break
 
-    env.close()
-
 if __name__ == '__main__':
-    env_main()
+    episodes_main()
